77ebd296f5906d947a24ef657ce12855bbd2e433
Lesedi.R <ramolotjalesedi@gmail.com>
2026-06-21 17:08:45 +0200

    Submitting server.js for review and advice

--- server.js
+++ server.js
@@ -44,6 +44,8 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     console.log('MongoDB connection error:', err.message);
     process.exit(1);
   });
+  //Temporary for any changes
+  
 
 
 
