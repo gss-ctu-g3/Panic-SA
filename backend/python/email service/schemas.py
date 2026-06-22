10063343537232e0adb29f18c1c64310be80345f
Crystal <thepcninja77@gmail.com>
2026-06-22 15:51:57 +0200

    Feat : Added consent email and other files

---
+++ backend/python/email service/schemas.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, Field
+
+class EmailPayload(BaseModel):
+    #i had it a typeddict but for validation and auto testing later this is better in long run
+    #from is resevered so we needed to do this
+    from_field: str = Field(alias="from") 
+    to: list[str]
+    subject: str
+    text: str | None = None
+    html: str
+
+class EmailConsent(BaseModel):
+    send_to: list[str]
+    user_full_name: str
+    revoke_consent_url: str
+
+
+class UserData(BaseModel):
+    username: str
+    email: str
+    age: int | None = None  # Optional field
+
+
+
